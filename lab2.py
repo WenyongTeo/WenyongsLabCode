9d340cb3ecaa77d49113624eb8006ae13904a54f
WenyongTeo <109842923+WenyongTeo@users.noreply.github.com>
2022-08-02 10:28:11 +0800

    Add files via upload

--- lab2.py
+++ lab2.py
@@ -1,7 +1,7 @@
 def main():
     name, email = str(input('Enter your email: ')).split('@')
     print("name is:", name)
-    print("Organisation is {0:<2s}".format(email))
+    print("Organisation is {}".format(email))
 
 
 main()
