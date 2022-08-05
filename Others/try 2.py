2350e26f1bfac8891e9906fb8d9975dc5803cab4
WenyongTeo <blue_wlng@hotmail.com>
2022-08-05 15:57:17 +0800

    Go

--- Others/try 2.py
+++ Others/try 2.py
@@ -8,21 +8,21 @@ def main():
 #    else:
 #        print('No package for you haha')
 
-#    year = int(input('Enter year '))
-#    if year < 1950:
-#        citizen= input("Are you a citizen before 1987?(y/n) ").upper()
-#        if citizen == 'y':
-#            print('Pioneer beetch')
-#        else:
-#            print('Nothing for you!')
-#    elif year >= 1950 and year < 1987:
-#        citizen = input("Are you a citizen before 1997?(y/n) ").upper()
-#        if citizen == 'y':
-#            print('Merdeka beetch')
-#        else:
-#            print('Nothing for you!')
-#    else:
-#        print('Nothing for you')
+    year = int(input('Enter year '))
+    if year < 1950:
+        citizen= input("Are you a citizen before 1987?(y/n) ").upper()
+        if citizen == 'y':
+            print('Pioneer beetch')
+        else:
+            print('Nothing for you!')
+    elif year >= 1950 and year < 1987:
+        citizen = input("Are you a citizen before 1997?(y/n) ").upper()
+        if citizen == 'y':
+            print('Merdeka beetch')
+        else:
+            print('Nothing for you!')
+    else:
+        print('Nothing for you')
 
 
 main()
