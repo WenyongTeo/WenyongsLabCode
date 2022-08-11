f68b54bd6f6711c04f7b248e326cf9ae320b8446
WenyongTeo <blue_wlng@hotmail.com>
2022-08-11 15:55:20 +0800

    This is lab2

--- Dr Angela Yu 100 Days of Code/Day 1.py
+++ Dr Angela Yu 100 Days of Code/Day 1.py
@@ -1,4 +1,5 @@
-#Printing, Commenting, Debugging,
-# String Manipulation and Variables
-#City you grew up with, pet name
-#= Band name
+#Write your code below this line 👇
+#input() will get user input in console
+#Then print() will print the word "Hello" and user input
+print("Hello" + "!" + input("what is your name?  "))
+print(len(input("What is your name? ")))
